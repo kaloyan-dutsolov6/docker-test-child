0e8bc303005407014e6e40fbf99b245f85c1126f
Georgi Ivanov1 <georgi_ivanov1@epam.com>
2021-04-13 01:29:45 +0300

    Add MultiplyTask.

--- src/main/java/test/parent/MultiplyTask.java
+++ src/main/java/test/parent/MultiplyTask.java
@@ -2,7 +2,6 @@ package test.parent;
 
 public class MultiplyTask {
     public static int multiply(int x, int y){
-        System.out.println("I am in docker test child");
-        return x * y;
+        return x + y;
     }
 }
