c2b509c84eee7e777455f6df8197061648e04c68
kaloyan-dutsolov6 <koki1231234@gmail.com>
2022-01-11 15:53:22 +0200

    Update Task.java

--- src/main/java/test/child/Task.java
+++ src/main/java/test/child/Task.java
@@ -2,10 +2,6 @@ package test.child;
 
 public class Task {
     public static int multiply(int x, int y){
-        boolean flag = true;
-        while(flag){
-            System.out.println("pet");
-        }
-        return x - y;
+        return x * y;
     }
 }
