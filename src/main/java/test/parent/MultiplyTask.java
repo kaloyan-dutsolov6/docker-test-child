03ac13cdc774c1b40b1b5d25ae44d79b28c74534
kaloyan-dutsolov6 <koki1231234@gmail.com>
2022-01-12 11:29:23 +0200

    Update MultiplyTask.java

--- src/main/java/test/parent/MultiplyTask.java
+++ src/main/java/test/parent/MultiplyTask.java
@@ -1,11 +1,11 @@
 package test.parent;
 
 public class MultiplyTask {
-    public static int multiply(int x, int y){
-        boolean flag = true;
-        while(flag){
-            System.out.println("pet");
-        }
+//     public static int multiply(int x, int y){
+//         boolean flag = true;
+//         while(flag){
+//             System.out.println("pet");
+//         }
         return x * y;
     }
 }
