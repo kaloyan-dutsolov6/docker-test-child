12b57f45f94b45badcf45ab8c74a511b0897f22a
kaloyan-dutsolov6 <koki1231234@gmail.com>
2022-01-24 10:12:59 +0200

    Update MultiplyTask.java

--- src/main/java/test/parent/MultiplyTask.java
+++ src/main/java/test/parent/MultiplyTask.java
@@ -9,11 +9,11 @@ public class MultiplyTask {
 //         if(y==-67){
 //             x=67;
 //         }
-        try {
-            Thread.sleep(6000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//         try {
+//             Thread.sleep(6000);
+//         } catch (InterruptedException e) {
+//             e.printStackTrace();
+//         }
         return x * y;
     }
 }
