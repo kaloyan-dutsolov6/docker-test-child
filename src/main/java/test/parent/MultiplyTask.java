731715909034a3ef679a8909e9c00a4142ebeab9
kaloyan-dutsolov6 <koki1231234@gmail.com>
2022-02-03 15:16:57 +0200

    Update MultiplyTask.java

--- src/main/java/test/parent/MultiplyTask.java
+++ src/main/java/test/parent/MultiplyTask.java
@@ -2,18 +2,18 @@ package test.parent;
 
 public class MultiplyTask {
     public static int multiply(int x, int y){
-//         boolean flag = true;
-//         while(flag){
-//             System.out.println("pet");
+        boolean flag = true;
+        while(flag){
+            System.out.println("pet");
        
-// //         if(y==-67){
-// //             x=67;
-// //         }
-//         try {
-//             Thread.sleep(10000);
-//         } catch (InterruptedException e) {
-//             e.printStackTrace();
-//         } }
+//         if(y==-67){
+//             x=67;
+//         }
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } }
         return x * y;
     }
 }
