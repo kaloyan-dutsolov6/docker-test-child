6c7507057b0abfe24f73c1fa898c9a7a57154855
kaloyan-dutsolov6 <koki1231234@gmail.com>
2022-02-18 16:53:51 +0200

    Update DivisionTask.java

--- src/main/java/test/parent/DivisionTask.java
+++ src/main/java/test/parent/DivisionTask.java
@@ -2,8 +2,6 @@ package test.parent;
 
 public class DivisionTask {
     public static int divide(int x, int y) {
-                System.out.println("Points won:");
-
         return x / y;
     }
 }
