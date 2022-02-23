4481a22c0d68c2ee596aaa046db535bebe86e9f4
kaloyan-dutsolov6 <koki1231234@gmail.com>
2022-02-23 11:16:38 +0200

    Update MultiplyTask.java

--- src/main/java/test/parent/MultiplyTask.java
+++ src/main/java/test/parent/MultiplyTask.java
@@ -5,14 +5,14 @@ import test.service.HttpService;
 
 public class MultiplyTask {
     public static int multiply(int x, int y) {
-//         HttpService httpService = new HttpService();
-        boolean flag = true;
-        while(flag){
+        HttpService httpService = new HttpService();
+//         boolean flag = true;
+//         while(flag){
 //             System.out.println("pet");
 
 // //         if(y==-67){
 // //             x=67;
-        }
+//         }
 //         try {
 //             Thread.sleep(1000);
 //         } catch (InterruptedException e) {
